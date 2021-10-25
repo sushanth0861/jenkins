8e4e359e974f3db4e4881afefe9af8ecf3cb4a82
Sushanth Ananthabhotla Venkata Subbu <sushanth.ananthabhotla@innovaccer.com>
2021-10-25 20:46:44 +0530

    Jenkins file name modified

--- sample.py
+++ sample.py
@@ -1,23 +1,23 @@
 import unittest
- 
+
 def car_price(cost):
-  if cost < 30000:
-    car_name= 'maruthi'
-  elif cost < 100000:
-    car_name = 'polo'
-  else:
-    car_name = 'merc'
-  return car_name
- 
- 
+	if cost < 30000:
+		car_name= 'maruthi'
+	elif cost < 100000:
+		car_name = 'polo'
+	else:
+		car_name = 'merc'
+	return car_name
+
+
 class NearestExitTests(unittest.TestCase):
- 
-  def test_row_1(self):
-    self.assertEqual(car_price(20000), 'maruthi', 'The nearest cost to row 1 is in the maruthi!')
-  def test_row_2(self):
-    self.assertEqual(car_price(50000), 'polo', 'The nearest cost to row 2 is in the polo!')
-  def test_row_3(self):s
-    self.assertEqual(car_price(150000), 'merc', 'The nearest cost to row 3 is in the merc!')
- 
- 
+
+	def test_row_1(self):
+		self.assertEqual(car_price(20000), 'maruthi', 'The nearest cost to row 1 is in the maruthi!')
+	def test_row_2(self):
+		self.assertEqual(car_price(50000), 'polo', 'The nearest cost to row 2 is in the polo!')
+	def test_row_3(self):
+		self.assertEqual(car_price(150000), 'merc', 'The nearest cost to row 3 is in the merc!')
+
+
 unittest.main()
